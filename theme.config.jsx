2dcc46829e87ac82c728bf2bc470aa92d3ab9459
maksim789456 <git@capybaraesttravy.ru>
2024-12-26 03:44:07 +0400

    Init (nextra, i18n)

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>ClientSamgk Docs</span>,
+    project: {
+      link: 'https://github.com/TheCrazyWolf/Api-Client-for-samgk.ru'
+    },
+    i18n: [
+      { locale: 'en', name: 'English' },
+      { locale: 'ru', name: 'Русский' },
+    ]
+  }
