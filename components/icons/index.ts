657098fc230adc7a1aa870d64f357ba8fcc9b8c8
maksim789456 <git@capybaraesttravy.ru>
2024-12-26 04:59:08 +0400

    Setup icons

---
+++ components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as TeacherIcon } from './teacher.svg'
+export { default as GroupsIcon } from './groups.svg'
+export { default as ScheduleIcon } from './schedule.svg'
+export { default as BuildingIcon } from './building.svg'
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,33 @@
+import path from 'node:path'
 import nextra from 'nextra'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
+  themeConfig: './theme.config.jsx',
+  search: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
 })
+
+const sep = path.sep === '/' ? '/' : '\\\\'
+const ALLOWED_SVG_REGEX = new RegExp(`components${sep}icons${sep}.+\\.svg$`)
 
 export default withNextra({
   i18n: {
     locales: ['en', 'ru'],
     defaultLocale: 'ru'
-  }
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    )
+    fileLoaderRule.exclude = ALLOWED_SVG_REGEX
+
+    config.module.rules.push({
+      test: ALLOWED_SVG_REGEX,
+      use: ['@svgr/webpack']
+    })
+    return config
+  },
 })
